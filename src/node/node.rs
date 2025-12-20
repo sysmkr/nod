b36fb3076d2073b91ac1ca58cc69b417f56c802d
sysmkr <to.sysmkr@gmail.com>
2025-12-20 18:27:57 +0100

    [node01] type testing setup

--- src/node/node.rs
+++ src/node/node.rs
@@ -1,11 +1,27 @@
 pub struct Node {
-    id: Option<u32>;
+    id: Option<u32>,
 }
 
 impl Node {
+    pub fn new() -> Self { Self { id: None } }
+    pub fn set_id(&mut self, id: u32) { self.id = Some(id); }
+    pub fn get_id(&self) -> Option<u32> { self.id }
+}
 
-    pub new() -> Self {
-        Self { id: None }
+#[cfg(test)]
+mod test_node_type {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let new_instance = Node::new();
+        assert_eq!(new_instance.get_id(), None);
     }
-
+    
+    #[test]
+    fn test_set_id() {
+        let mut new_instance = Node::new();
+        new_instance.set_id(10);
+        assert_eq!(new_instance.get_id(), Some(10));
+    }
 }
