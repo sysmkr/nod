6f6a7d93f28d7e022e82d74b8d8d75c875d12e7b
sysmkr <to.sysmkr@gmail.com>
2025-12-21 02:40:51 +0100

    [node04] node.erase

---
+++ src/node/erase.rs
@@ -0,0 +1,23 @@
+use super::Node;
+
+impl Node {
+    pub fn erase(&mut self, key: &String) {
+        self.get_storage().remove(key);
+    }
+}
+
+#[cfg(test)]
+mod erase {
+    use super::Node;
+
+    #[test]
+    fn test() {
+        let mut new_instance = Node::new();
+        let key = String::from("test.txt");
+        let value = String::from("Hello.");
+        new_instance.store(key.clone(), value.clone());
+        assert_eq!(new_instance.request(&key), Some(value.clone()).as_ref());
+        new_instance.erase(&key);
+        assert_eq!(new_instance.request(&key), None);
+    }
+}
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,5 +1,6 @@
 pub mod structure;
 mod store;
 mod request;
+mod erase;
 
 pub use structure::Node as Node;
